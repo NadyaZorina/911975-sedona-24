585e1e2679b31fde0243082ca24c9c32720460e2
NadyaZorina <46607567+NadyaZorina@users.noreply.github.com>
2019-03-23 23:21:50 +0300

    Финальные правки

--- js/popup.js
+++ js/popup.js
@@ -2,47 +2,41 @@
   var popup = document.querySelector(".appointment-form");
   var form = document.querySelector("form");
   var arrival = popup.querySelector("[name=arrival-date]");
-  var departure = popup.querySelector("[name=departure-date]"); 
+  var departure = popup.querySelector("[name=departure-date]");
   var adult = popup.querySelector("[name=adult]");
   var child = popup.querySelector("[name=child]");
   var isStorageSupport = true;
   var storage = "";
-  
   try {
     storage = localStorage.getItem("adult");
-  }
-  catch (err) {
+  } catch (err) {
     isStorageSupport = false;
   }
   if (storage) {
     adult.value = storage;
     departure.focus();
-    } else {
+  } else {
     arrival.focus();
-    }
-           
-     link.addEventListener("click", function (evt) {
-       evt.preventDefault();
-       popup.classList.toggle("modal-show");
+  }
+  link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-show");
   });
-
-       form.addEventListener("submit", function (evt) {
-    if (!arrival.value || !departure.value || !adult.value || !child.value ) {
+  form.addEventListener("submit", function(evt) {
+    if (!arrival.value || !departure.value || !adult.value || !child.value) {
       evt.preventDefault();
       popup.classList.remove("modal-error");
       popup.offsetWidth = popup.offsetWidth;
       popup.classList.add("modal-error");
       console.log("Необходимо ввести данные для поиска");
-    }
-    else {
-      popup.classList.remove("modal-error");  
+    } else {
+      popup.classList.remove("modal-error");
       if (isStorageSupport) {
         localStorage.setItem("adult", adult.value);
       }
     }
-  }); 
-
-  window.addEventListener("keydown", function (evt) {
+  });
+  window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
       if (popup.classList.contains("modal-show")) {
@@ -50,4 +44,4 @@
         popup.classList.remove("modal-error");
       }
     }
-  });          
+  });
